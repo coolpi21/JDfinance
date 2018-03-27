287865b5e7eb4c490f1ac34ee99f107cecc2c03c
coolpi07 <coolpi07@gmail.com>
2018-03-27 11:02:39 +0800

    添加理财精选模块

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -4,6 +4,7 @@
         <Slider/>
         <Novice/>
         <Borrow/>
+        <Money/>
     </div>
 </template>
 
@@ -12,12 +13,14 @@ import Heador from '../public/header.vue';
 import Slider from './hslider.vue';
 import Novice from './novice.vue';
 import Borrow from './borrow.vue';
+import Money from './money.vue';
 export default {
   components:{
     Heador,
     Slider,
     Novice,
     Borrow,
+    Money,
   }
 }
 </script>
