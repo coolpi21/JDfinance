36e691be8e52ec760b6d202a820921beaccabc48
coolpi07 <coolpi07@gmail.com>
2018-03-29 14:29:57 +0800

    完成众筹轮播组件

--- app/js/raise/rslider.vue
+++ app/js/raise/rslider.vue
@@ -1,48 +1,72 @@
 <template>
   <Panel :class="$style.panel" title="轮播图">
     <section :class="$style.content">
-      <Slider :items="items" :cname="$style.slider"></Slider>
+      <swiper :options="options" :class="$style.slider">
+        <swiper-slide :class="$style.item">签到领流量，1元500M！<em> > </em></swiper-slide>
+        <swiper-slide :class="$style.item">百万白条券免费送，速来领取！！  <em> > </em></swiper-slide>
+        
+      </swiper>
     </section>
   </Panel>
 </template>
 
 <script>
-import Panel from '../core/panel';
-import Slider from '../core/slider';
+import Panel from "../core/panel";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
   components: {
     Panel,
-    Slider
+    swiper,
+    swiperSlide
   },
-  data(){
+  data() {
     return {
-      items: [{
-        href: 'ious',
-        src: '//img12.360buyimg.com/jrpmobile/jfs/t8491/303/504628479/60643/100648de/59a94358N92475fca.jpg?width=750&height=320'
-      },
-      {
-        href: 'ious',
-        src: '//img12.360buyimg.com/jrpmobile/jfs/t8437/321/1758527686/40952/f66d552f/59bf1f41N50d846f9.jpg?width=750&height=320'
-      },
-      ]
+      options: {
+        loop: true,
+        autoplay: true,
+        direction: "vertical"
+      }
+    };
+  }
+};
+</script>
+<style lang="scss">
+.swiper-container-vertical {
+  height: 72px;
+  .swiper-slide {
+    em {
     }
   }
-
 }
-</script>
-
+</style>
 <style lang="scss" module>
 @import "../../css/element.scss";
 .panel {
   @include panel;
-  >h4 {
+  > h4 {
     display: none;
   }
-  .content{
+  .content {
+    overflow: hidden;
+    margin-top: 120px;
+    padding-left: 166px;
+    background: url(//m.jr.jd.com/spe/qyy/main/images/scroll_title_img.png) left
+      top no-repeat;
+    height: 72px;
+    line-height: 72px;
     .slider {
-       margin-top: 120px;
+      
+      font-size: 26px;
+      color: #333;
+      .item {
+        position: relative;
+        > em {
+          color: #ddd;
+          position: absolute;
+          right: 20px;
+        }
+      }
     }
   }
 }
-
 </style>
